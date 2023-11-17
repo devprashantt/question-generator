a7c6fef945c32bb90f1c9a289cd0fc75609c8cb0
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-11-17 15:12:14 +0530

    Add question generation logic to questionController.js

--- server/src/controllers/questionController.js
+++ server/src/controllers/questionController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const QuestionService = require("../services/questionService");
+const generateQuestionPaper = require("../utils/questionGenerator");
 
 const router = express.Router();
 router.use(bodyParser.json());
@@ -33,46 +34,5 @@ router.post("/generate", (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-function generateQuestionPaper(totalMarks, difficultyDistribution, questions) {
-  const questionPaper = [];
-
-  for (const difficulty in difficultyDistribution) {
-    if (difficultyDistribution.hasOwnProperty(difficulty)) {
-      const percentage = difficultyDistribution[difficulty];
-      const marksForDifficulty = Math.floor((totalMarks * percentage) / 100);
-
-      console.log("marking for difficulty", marksForDifficulty);
-
-      // Calculate remaining marks for other difficulty levels
-      const remainingMarks =
-        totalMarks - questionPaper.reduce((sum, q) => sum + q.marks, 0);
-
-      console.log("remaining marks", remainingMarks);
-
-      // Ensure marksForDifficulty is within the remaining marks
-      const actualMarksForDifficulty = Math.min(
-        marksForDifficulty,
-        remainingMarks
-      );
-
-      console.log("actualMarksForDifficulty", actualMarksForDifficulty);
-
-      if (actualMarksForDifficulty > 0) {
-        const filteredQuestions = questions.filter(
-          (q) => q.difficulty === difficulty
-        );
-        const selectedQuestions = filteredQuestions.slice(
-          0,
-          actualMarksForDifficulty
-        );
-
-        questionPaper.push(...selectedQuestions);
-      }
-    }
-  }
-
-  return questionPaper;
-}
 
 module.exports = router;
--- server/src/utils/questionGenerator.js
+++ server/src/utils/questionGenerator.js
@@ -6,12 +6,36 @@ function generateQuestionPaper(totalMarks, difficultyDistribution, questions) {
       const percentage = difficultyDistribution[difficulty];
       const marksForDifficulty = Math.floor((totalMarks * percentage) / 100);
 
-      const filteredQuestions = questions.filter(
+      // Get questions for this difficulty level
+      const questionsForDifficulty = questions.filter(
         (q) => q.difficulty === difficulty
       );
-      const selectedQuestions = filteredQuestions.slice(0, marksForDifficulty);
 
-      questionPaper.push(...selectedQuestions);
+      // Sort questions by marks in ascending order
+      questionsForDifficulty.sort((a, b) => a.marks - b.marks);
+
+      // Push for this difficulty level in questionPaper and sum should be less than marksForDifficulty
+      let sum = 0;
+      for (const question of questionsForDifficulty) {
+        if (sum + question.marks <= marksForDifficulty) {
+          questionPaper.push(question);
+          sum += question.marks;
+        }
+      }
+
+      // If sum is less than marksForDifficulty, then we need to add more questions from same level randomly
+      if (sum < marksForDifficulty) {
+        while (sum < marksForDifficulty) {
+          // Add remainingQuestions randomly
+          const randomIndex = Math.floor(
+            Math.random() * questionsForDifficulty.length
+          );
+          const randomQuestion = questionsForDifficulty[randomIndex];
+          console.log(randomQuestion);
+          questionPaper.push(randomQuestion);
+          sum += randomQuestion.marks;
+        }
+      }
     }
   }
 
